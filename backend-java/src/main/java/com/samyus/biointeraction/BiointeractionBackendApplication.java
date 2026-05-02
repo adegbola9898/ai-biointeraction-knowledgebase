90d777ca07c623dc7f35e609a71e59b3f062c097
Samuel <adegbola9898@run.edu.ng>
2026-05-02 20:15:48 +0100

    Sprint 2: initialize Spring Boot backend

---
+++ backend-java/src/main/java/com/samyus/biointeraction/BiointeractionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.samyus.biointeraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiointeractionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiointeractionBackendApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/samyus/biointeraction/BiointeractionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samyus.biointeraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiointeractionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
